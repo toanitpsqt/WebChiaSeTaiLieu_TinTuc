9beb5fbada6d6380db43879f26363710d490da58
ducthangtit <leducthang93it@gmail.com>
2017-02-25 22:39:06 +0700

    push html/css

---
+++ WebChiaSeTaiLieu_TinTuc/src/main/java/tdevteam/form/Contact.java
@@ -0,0 +1,9 @@
+package tdevteam.form;
+
+public class Contact {  
+    private String firstname;  
+    private String lastname;  
+    private String email;  
+    private String telephone;  
+    //getters and setters  
+}  
