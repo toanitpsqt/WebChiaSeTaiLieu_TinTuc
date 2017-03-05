26e1a35b9915fb2f0ee380fc36d3fb8c0399de53
toanitpsqt <nguyendinhtoanps@gmail.com>
2017-03-05 23:50:34 +0700

    ewewew

--- WebChiaSeTaiLieu_TinTuc/src/main/java/tdevteam/form/Contact.java
+++
@@ -1,9 +0,0 @@
-package tdevteam.form;
-
-public class Contact {  
-    private String firstname;  
-    private String lastname;  
-    private String email;  
-    private String telephone;  
-    //getters and setters  
-}  
--- WebChiaSeTaiLieu_TinTuc/src/main/webapp/WEB-INF/views/pagesAdmin/contactus.jsp
+++
@@ -1,7 +0,0 @@
-Contact Us: webtintuc
-<br>
-<%-- Address: ${address} --%>
-<br>
-<%-- Phone: ${phone} --%>
-<br>
-<%-- Email: ${email} --%>
